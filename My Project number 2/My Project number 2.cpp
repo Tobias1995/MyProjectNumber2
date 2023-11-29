f46e8e7039076da0d18554a3073c22a253df0930
Бехруз <@LAPTOP-4TGQKAG1>
2023-11-29 13:20:41 +0700

    Добавьте файлы проекта.

---
+++ My Project number 2/My Project number 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    string surname;
+    int age;
+    cout << "Please ,enter your name \n";
+    cin >> name;
+    cout << name << "   " << "please , enter your surname\n";
+    cin >> surname;
+    cout << name <<"  " << surname<<"  "  << "please ,enter how old are you \n";
+    cin >> age;
+    cout << "Hello " << name << "  " << surname << " " << "thank you for information";
+}
+    
